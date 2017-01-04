5a02c6622e838468fd960d9707e3acdcaa1ca861
zhaoyc <zhaoyc@study.com>
2017-01-04 16:29:40 +0800

    完成了退出登录

--- public/src/common.js
+++ public/src/common.js
@@ -2,9 +2,24 @@
 // 将原来代码包装成模块
 // 将原来代码用define方法包起就可以了
 define(['jquery', 'template', 'cookie'], function ($, template) {
+
+	// 退出登录
+	$('#logout').on('click', function () {
+		// confirm('确定要退出吗');
+		$.ajax({
+			url: '/api/logout',
+			type: 'post',
+			success: function (info) {
+				// console.log(info);
+				if(info.code == 200) {
+					location.href = '/login';
+				}
+			}
+		});
+	});
+
 	// 通过检测cookie中有没有PHPSESSID来判断用户是否登录
 	// 除了登录页面以外都要进行检测
-
 	var pathname = location.pathname;
 	if(pathname != '/index.php/login' && !$.cookie('PHPSESSID')) {
 		location.href = '/index.php/login';
